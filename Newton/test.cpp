37071b8771cb804e1f86a9879715f33e3b37cce3
arukadya <a19.sj7x@g.chuo-u.ac.jp>
2022-12-17 21:13:29 +0900

    add test.cpp

---
+++ Newton/test.cpp
@@ -0,0 +1,8 @@
+//
+//  test.cpp
+//  Newton
+//
+//  Created by 須之内俊樹 on 2022/12/17.
+//
+
+#include "test.hpp"
---
+++ Newton/test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  Newton
+//
+//  Created by 須之内俊樹 on 2022/12/17.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
